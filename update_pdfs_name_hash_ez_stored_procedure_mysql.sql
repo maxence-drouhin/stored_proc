1daffe0a7cddb3d0c075f2fc0dc60b936c27364f
Maxence <maxence.drouhin@gmail.com>
2018-08-08 17:14:59 +0200

    code indent

--- update_pdfs_name_hash_ez_stored_procedure_mysql.sql
+++ update_pdfs_name_hash_ez_stored_procedure_mysql.sql
@@ -10,9 +10,7 @@ CREATE PROCEDURE updatenamehash()
   DECLARE maxLoop INT;
   DECLARE counter INT;
   DECLARE vfinished INTEGER DEFAULT 0;
-  
- 
-  
+
   DECLARE cur CURSOR FOR SELECT name_hash FROM ezdbfile;
   DECLARE CONTINUE HANDLER FOR NOT FOUND SET vfinished = 1;
 
@@ -27,38 +25,35 @@ CREATE PROCEDURE updatenamehash()
 	SET FOREIGN_KEY_CHECKS = 0;
 	
 	update ezdbfile
-	set name = replace(name,'var/pam', 'var/nancy')
+	set name = replace(name,'var/oldpath', 'var/newPath')
 	WHERE scope = 'binaryfile';
 
 	update ezdbfile
-	set name_trunk = replace(name_trunk,'var/pam', 'var/nancy')
+	set name_trunk = replace(name_trunk,'var/oldpath', 'var/NewPath')
 	WHERE scope = 'binaryfile';
-
-	  
   
   OPEN cur; 
   SET counter = 1;
   updateNameHashLoop: LOOP
-	FETCH cur INTO ezdbfile_name_hash;
-    SELECT count(name_hash) FROM ezdbfile INTO maxLoop;	
-	
-	IF vfinished = 1 THEN
-      LEAVE updateNameHashLoop;
-	END IF;
-	
+    FETCH cur INTO ezdbfile_name_hash;
+      SELECT count(name_hash) FROM ezdbfile INTO maxLoop;
+
+    IF vfinished = 1 THEN
+        LEAVE updateNameHashLoop;
+    END IF;
+
     IF counter > maxLoop THEN
      LEAVE updateNameHashLoop;
     END IF;
 	
 	SELECT name_trunk FROM ezdbfile WHERE name_hash = ezdbfile_name_hash INTO ezdbfile_name_trunk;
-	
-    SELECT count(name_hash) FROM ezdbfile WHERE name_hash = md5(ezdbfile_name_trunk) INTO check_new_name_hash_exists;
+	SELECT count(name_hash) FROM ezdbfile WHERE name_hash = md5(ezdbfile_name_trunk) INTO check_new_name_hash_exists;
 	
 	IF check_new_name_hash_exists = 0  THEN
-	SET ezdbfile_data_new_name_trunk = replace(ezdbfile_name_trunk,'var/nancy', 'var/pam');
-	INSERT INTO ezdbfile_data_new_name_hash VALUES ( ezdbfile_name_trunk, ezdbfile_data_new_name_trunk, md5(ezdbfile_name_trunk), md5(ezdbfile_data_new_name_trunk) );
-	UPDATE ezdbfile_data SET name_hash = md5(ezdbfile_name_trunk) WHERE name_hash = md5(ezdbfile_data_new_name_trunk);
-	UPDATE ezdbfile SET name_hash = md5(ezdbfile_name_trunk) WHERE name_hash = md5(ezdbfile_data_new_name_trunk);
+    SET ezdbfile_data_new_name_trunk = replace(ezdbfile_name_trunk,'var/newPath', 'var/oldPath');
+    INSERT INTO ezdbfile_data_new_name_hash VALUES ( ezdbfile_name_trunk, ezdbfile_data_new_name_trunk, md5(ezdbfile_name_trunk), md5(ezdbfile_data_new_name_trunk) );
+    UPDATE ezdbfile_data SET name_hash = md5(ezdbfile_name_trunk) WHERE name_hash = md5(ezdbfile_data_new_name_trunk);
+    UPDATE ezdbfile SET name_hash = md5(ezdbfile_name_trunk) WHERE name_hash = md5(ezdbfile_data_new_name_trunk);
 	END IF;
   
   SET counter = counter + 1; 
